d02c7e82d322ee546031c1067d7b10bee26e1f89
Alan Davidson <alan@viam.com>
2024-02-20 18:47:10 -0500

    make a way to store and restore old state

--- st/overrides.go
+++ st/overrides.go
@@ -2,7 +2,66 @@ package st
 
 import (
 	"context"
+	"fmt"
+
+	"go.uber.org/multierr"
 )
+
+type OldState struct {
+	acceleration string
+	deceleration string
+	// Perhaps more parameters will go here.
+}
+
+func SetOverrides(
+	ctx context.Context, comms CommPort, extra map[string]interface{},
+) (OldState, error) {
+	var err error
+
+	// This function does the heavy lifting of writing to the device and updating err. It returns
+	// values to put into the old state.
+	store := func (key, command string) string {
+		val, exists := extra[key]
+		if !exists {
+			return "" // Use the default
+		}
+
+		realVal, ok := val.(float32)
+		if !ok {
+			err = multierr.Combine(err, fmt.Errorf("malformed value for %s: %#v", key, val))
+			return ""
+		}
+		response, sendErr := ReplaceValue(ctx, comms, fmt.Sprintf("%s%.3f", command, realVal))
+		err = multierr.Combine(err, sendErr)
+		if response[:3] != command + "=" {
+			err = multierr.Combine(err, fmt.Errorf("unexpected response when storing %s: %#v",
+												   key, response))
+			return ""
+		}
+		return response[3:]
+	}
+
+	var os OldState
+	os.acceleration = store("acceleration", "AC")
+	os.deceleration = store("deceleration", "DE")
+	return os, err
+}
+
+func (os *OldState) Restore(ctx context.Context, comms CommPort) error {
+	// This function does all the heavy lifting of restoring the old state.
+	restore := func (command, value string) error {
+		if value == "" {
+			return nil // No old state stored
+		}
+		_, err := comms.Send(ctx, command + value)
+		return err
+	}
+
+	return multierr.Combine(
+		restore("AC", os.acceleration)
+		restore("DE", os.deceleration)
+	)
+}
 
 // ReplaceValue first sends on the CommPort a version of the command with no arguments, then the
 // entire command, and returns what it received from the first one. It is intended to be used to
